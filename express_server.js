5c9aef10a2ca473a1ace2253e1b57864b9ff3bd4
stella-zb <upperdaystudio@gmail.com>
2019-10-29 02:59:22 +0000

    setup route for GET and Post request, install body-parser as middleware

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -26,10 +29,19 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL }
   res.render('urls_show', templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
