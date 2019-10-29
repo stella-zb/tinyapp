a54bc48e8cb6df1bc090aa980bd8c849d5bc38bb
stella-zb <upperdaystudio@gmail.com>
2019-10-29 19:24:59 +0000

    redirect the shortURL to longURL

--- express_server.js
+++ express_server.js
@@ -54,6 +54,10 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
