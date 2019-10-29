5258eb45334cd9c7b5531a2c2f8ce33e23424a56
stella-zb <upperdaystudio@gmail.com>
2019-10-29 23:34:19 +0000

    eslint the express_server.js

--- express_server.js
+++ express_server.js
@@ -12,45 +12,31 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-function generateRandomString() {
+const generateRandomString = () => {
   let randomString = '';
   const charset = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 6; i++)
+  for (let i = 0; i < 6; i++) {
     randomString += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
   return randomString;
 };
-
-// app.get('/', (req, res) => {
-//   res.send('Hello!');
-// });
-
-// app.get('/urls.json', (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n');
-// });
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body); 
-  let randomURL = generateRandomString(); 
+  let randomURL = generateRandomString();
   urlDatabase[randomURL] = req.body.longURL;
-  console.log(urlDatabase);
   res.redirect(`/urls/${randomURL}`);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  console.log(templateVars);
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
 
