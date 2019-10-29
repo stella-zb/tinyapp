3692c18ef0b5eb5e8c813d1d24283acdff5e52cc
stella-zb <upperdaystudio@gmail.com>
2019-10-29 23:26:25 +0000

    add Edit button, add form for update longURL, add post route for update resource

--- express_server.js
+++ express_server.js
@@ -55,8 +55,14 @@ app.get('/urls/:shortURL', (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  let editURL = req.params.shortURL;
+  urlDatabase[editURL] = req.body.longURL;
+  res.redirect('/urls');
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
