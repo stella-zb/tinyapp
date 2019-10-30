bab7b3e4ac04b3bdf39770c0591d42c11861be0f
stella-zb <upperdaystudio@gmail.com>
2019-10-30 03:54:56 +0000

    add login route, add logout route, Pass in the username to all views that include the _header.ejs partial

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -22,21 +24,42 @@ const generateRandomString = () => {
 };
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies['username']
+  };
+  console.log(templateVars);
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies['username']
+  };
+  res.render('urls_new', templateVars);
 });
+
 app.post("/urls", (req, res) => {
   let randomURL = generateRandomString();
   urlDatabase[randomURL] = req.body.longURL;
   res.redirect(`/urls/${randomURL}`);
 });
 
+app.post('/login', (req,res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+app.post('/logout', (req,res) => {
+  res.clearCookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { 
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username']
+  };
   res.render('urls_show', templateVars);
 });
 
